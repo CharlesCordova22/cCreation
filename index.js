f9f602a00ace57a89104fa2a204e47caa17ce0fd
Charles Cordova <131734392+CharlesCordova22@users.noreply.github.com>
2025-04-19 00:20:09 +0800

    sss

--- index.js
+++ index.js
@@ -12,32 +12,3 @@ function toggleMenu() {
   const menu = document.getElementById("menu");
   menu.classList.toggle("show");
 }
-
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwy3I0f_-9GnuuVvvJZy4nQr8C0MJL6benVn2xpFWoM0l94Qc4lPuJs3ArdapHOnswx/exec';
-
-  document.getElementById('contactForm').addEventListener('submit', e => {
-    e.preventDefault();
-
-    const form = e.target;
-    const data = {
-      name: form.name.value,
-      email: form.email.value,
-      message: form.message.value
-    };
-
-    fetch(scriptURL, {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(response => response.json())
-    .then(result => {
-      alert("Message sent successfully!");
-      form.reset();
-    })
-    .catch(error => {
-      console.error('Error!', error.message);
-    });
-  });
